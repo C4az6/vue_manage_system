7796e958458b5e10109d6cc07b308c5f20676007
C4az6 <1026037632@qq.com>
2019-09-24 20:37:09 +0800

    登录组件初始化完成

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import '@/styles/index.less'
+
 import Vue from 'vue'
 import App from './App.vue'
 // 引入路由模块
 import router from '@/router.js'
+// 引入element-ui框架
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,14 +1,84 @@
 <template>
-  <div id="login">
-    登录
+  <div class="login">
+    <div class="project-name">Vue后台管理系统</div>
+    <div class="container">
+      <img src="../assets/avatar2.jpg" alt class="avatar" />
+      <el-form :model="loginForm" :rules="rules" ref="loginForm" class="demo-loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" clearable placeholder="请输入用户名"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" clearable placeholder="请输入密码"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" class="login-btn">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  // data是一个函数,返回一个对象
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      // 添加验证规则
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  }
 }
 </script>
 
-style:<style lang="less" scoped>
-
+<style lang="less" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: url("../assets/bg.jpg");
+  .project-name {
+    width: 100%;
+    // background-color: red;
+    font-size: 28px;
+    height: 100px;
+    line-height: 300px;
+    color: #fff;
+    text-align: center;
+  }
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
 </style>
