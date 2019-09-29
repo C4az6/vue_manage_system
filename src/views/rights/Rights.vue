3d8e3764002e532b13da82634a177f7e37bcc8ff
C4az6 <1026037632@qq.com>
2019-09-30 04:00:51 +0800

    完成权限列表页面表格数据的动态渲染

--- src/views/rights/Rights.vue
+++ src/views/rights/Rights.vue
@@ -8,12 +8,56 @@
     </el-breadcrumb>
 
     <!-- 表格部分 -->
-
+    <el-table :data="allRightsList" style="width: 100%" border>
+      <el-table-column type="index"></el-table-column>
+      <el-table-column prop="authName" label="权限" width="280"></el-table-column>
+      <el-table-column prop="path" label="路径" width="280"></el-table-column>
+      <el-table-column prop="level" label="层级">
+        <template slot-scope="scope">
+          <span>{{scope.row.level | levelFilter}}</span>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-export default {}
+import { getAllRightsApi } from '@/api/rights.js'
+export default {
+  // 添加局部过滤器
+  filters: {
+    levelFilter: (level) => {
+      // level其实就是scope.row.level
+      if (level === '0') {
+        return '一级'
+      } else if (level === '1') {
+        return '二级'
+      } else if (level === '2') {
+        return '三级'
+      }
+      return ''
+    }
+  },
+  data () {
+    return {
+      allRightsList: []
+    }
+  },
+  methods: {
+    getAllRight () {
+      getAllRightsApi('list')
+        .then(res => {
+          this.allRightsList = res.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  mounted () {
+    this.getAllRight()
+  }
+}
 </script>
 
 <style lang="less" scoped>
