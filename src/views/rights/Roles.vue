95389cdaad86b6c1270abf6d13d72489254b7e6e
C4az6 <1026037632@qq.com>
2019-09-30 05:39:00 +0800

    完成角色列表展开行数据的动态渲染

--- src/views/rights/Roles.vue
+++ src/views/rights/Roles.vue
@@ -6,16 +6,77 @@
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>角色列表</el-breadcrumb-item>
     </el-breadcrumb>
+    <!-- 添加角色按钮 -->
     <el-button type="success" plain>添加角色</el-button>
+
+    <!-- 展开行表格 -->
+    <el-table :data="rolesList" style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <!-- 使用element-ui的栅格系统实现 -->
+          <el-row v-for="first in scope.row.children" :key="first.id" style="margin-bottom:15px">
+            <el-col :span="4">
+              <el-tag closable type="success">{{first.authName}}</el-tag>
+            </el-col>
+            <el-col :span="20">
+              <el-row v-for="second in first.children" :key="second.id">
+                <el-col :span="4">
+                   <el-tag closable type="info">{{second.authName}}</el-tag>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag closable type="danger" v-for="third in second.children" :key="third.id">{{third.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
+      <el-table-column type="index"></el-table-column>
+      <el-table-column label="角色名称" prop="roleName"></el-table-column>
+      <el-table-column label="描述" prop="roleDesc"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+            <el-button type="info" icon="el-icon-edit"></el-button>
+          </el-tooltip>
+
+          <el-tooltip class="item" effect="dark" content="角色授权" placement="top">
+            <el-button type="success" icon="el-icon-share"></el-button>
+          </el-tooltip>
+
+          <el-tooltip class="item" effect="dark" content="删除" placement="top">
+            <el-button type="warning" icon="el-icon-delete"></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import { getRolesListApi } from '@/api/roles.js'
 export default {
-
+  data () {
+    return {
+      rolesList: []
+    }
+  },
+  methods: {
+    getAllRoles () {
+      getRolesListApi()
+        .then(res => {
+          this.rolesList = res.data.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  mounted () {
+    this.getAllRoles()
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
